ff672d1875cc55e97616ef86a3e64c2dcace74f8
animeshjoshi <animeshjoshi88@gmail.com>
2025-09-21 17:27:23 -0400

    Compute KDE based tolerance intervals

--- tolerance_intervals.py
+++ tolerance_intervals.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import norm
 from scipy.stats import chi2
 from scipy.stats import binom
-
+import openturns as ot
 
 def compute_normal_ti(data, proportion, confidence):
 
@@ -62,10 +62,99 @@ def compute_bootstrap_ti(data, proportion, confidence):
         lower_bounds.append(compute_nonparametric_ti(sample,proportion,confidence)[0])
         upper_bounds.append(compute_nonparametric_ti(sample,proportion,confidence)[1])
 
-    lower = np.percentile(lower_bounds, (1-confidence)/2)
-    upper = np.percentile(upper_bounds, confidence/2)
+    lower = np.percentile(lower_bounds, 100*((1-confidence)/2))
+    upper = np.percentile(upper_bounds, 100*(1+confidence/2))
 
     return (lower,upper)
+
+
+def compute_bootstrap_kde_ti(data, proportion, confidence):
+
+    np.random.seed(15)
+
+    lower_bounds = []
+    upper_bounds = []
+
+    for x in range(10000):
+
+        sample = np.random.choice(data,size=len(data), replace = True)
+        sample = ot.Sample(sample, 1)
+
+        factory = ot.KernelSmoothing(kernel = ot.Normal())
+
+        bandwidth = factory.computePluginBandwidth(sample)
+
+        distribution = factory.build(sample, bandwidth)
+
+
+
+        lower_bounds.append(distribution.computeQuantile(0.025)[0])
+        upper_bounds.append(distribution.computeQuantile(0.975)[0])
+
+    lower = np.percentile(lower_bounds, 100*((1-confidence)/2))
+    upper = np.percentile(upper_bounds, 100*(1+confidence/2))
+
+    return (lower,upper)
+
+
+def find_sheather_bandwith(sample):
+    
+    sample = ot.Sample(sample,1)
+    factory = ot.KernelSmoothing(kernel = ot.Normal())
+    h = factory.computePluginBandwidth(sample)[0]
+    return h
+
+def compute_bootstrap_kde_shrunk_smooth(data, proportion, confidence):
+
+    np.random.seed(15)
+
+    lower_bounds = []
+    upper_bounds = []
+
+    ybar = np.mean(data)
+
+    for x in range(10000):
+
+        sample = np.random.choice(data,size=len(data), replace = True)
+        h = find_sheather_bandwith(sample)
+        ystarbar = np.mean(sample)
+        n = len(data)
+
+        
+        second_moment = 1
+
+        denom = np.sum((data-ybar)**2)
+        v = (1 + (n * h**2 * second_moment) / denom) ** (-0.5)
+
+        delta = np.random.normal(size=n) 
+        
+        new_sample = (1 - v) * ystarbar + v * sample + h * v * delta
+
+
+
+
+
+        sample = ot.Sample(new_sample, 1)
+        factory = ot.KernelSmoothing(kernel = ot.Normal())
+
+        bandwidth = factory.computePluginBandwidth(sample)
+
+        distribution = factory.build(sample, h)
+
+
+
+        lower_bounds.append(distribution.computeQuantile(0.025)[0])
+        upper_bounds.append(distribution.computeQuantile(0.975)[0])
+
+    lower = np.percentile(lower_bounds, 100*((1-confidence)/2))
+    upper = np.percentile(upper_bounds, 100*(1+confidence/2))
+
+    return (lower,upper)
+
+
+
+
+
 
 
 
